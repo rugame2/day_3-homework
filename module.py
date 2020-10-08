d67da0396e4bcaf98dabb3c8cc7b955285319c98
rugame2 <idamccarty@mindspring.com>
2020-10-08 01:29:59 -0500

    first commit

---
+++ module.py
@@ -0,0 +1,9 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
+    
+def printSquareFootage(name):
+    print(f'Hello {sq_footage}...of a house is")
+          
+def printCircumference(name):
+    print(f'Hello {circumference of circle}... is")
